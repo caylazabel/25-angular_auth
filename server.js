35d612e23950c0a3e345009bb12d0954ba6361cb
caylazabel <caylazabel@gmail.com>
2017-04-11 13:12:08 -0700

    travis file

--- server.js
+++ server.js
@@ -2,3 +2,10 @@
 
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(`${__dirname}/build`));
+
+app.listen(PORT, function(){
+  console.log('server up:', PORT);
+});
